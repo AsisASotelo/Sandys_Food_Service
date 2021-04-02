03cc4f6760b590f4b9e2ed3ce9fb32098b441793
AsisASotelo <asis.a.sotelo@gmail.com>
2021-04-01 20:37:55 -0700

    Add Model View

---
+++ model_view_ex.py
@@ -0,0 +1,9 @@
+# model_view_ex.py
+# Asis Sotelo
+#
+# 04April21
+
+import sys, csv
+from PyQt.QtWidgets import (QApplication,QtWidgets,QTableView,QVBoxLayout)
+from PyQt.QtGui import QStandardItemModel,QStandardItemModel
+
